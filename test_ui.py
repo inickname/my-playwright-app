5bf028207911f15fbbc9a45452d3a7ef7d7f1441
inickname <nikolaysmorodinov1@gmail.com>
2025-09-02 23:58:16 +0300

    Добавлены новые тесты

---
+++ test_ui.py
@@ -0,0 +1,15 @@
+from pages.checkout_page import CheckoutPage
+from pages.inventory_page import InventoryPage
+from pages.login_page import LoginPage
+
+
+def test_checkout_order(browser):
+    page = browser.new_page()
+    login_page = LoginPage(page)
+    inventory_page = InventoryPage(page)
+    checkout_page = CheckoutPage(page)
+
+    login_page.login('standard_user', 'secret_sauce')
+    inventory_page.add_first_item_to_cart()
+    checkout_page.start_checkout()
+    checkout_page.fill_checkout_form('John', 'Doe', '12345')
